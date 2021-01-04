5779e113fa5f91b8b31b957801358f2482017d70
Rumpca <Jakub.Rumpca@finastra.com>
2021-01-04 14:23:51 +0100

    added globby

--- PublishHtmlReport/index.js
+++ PublishHtmlReport/index.js
@@ -1,17 +1,51 @@
 const tl = require('azure-pipelines-task-lib');
 const { resolve } = require('path');
 const dashify = require('dashify')
+const globby = require('globby')
 
-try {
+
+function run () {
     let reportDir = tl.getPathInput('reportDir', true, true);
-    console.log(reportDir)
+  
+    let files = globby.sync([reportDir.replace(/\\/g, '/')], {expandDirectories : {files: ['*'], extensions: ['html']}})
+  
+    const fileProperties = []
+  
+    files.forEach(file => {
+      tl.debug(`Reading report ${file}`)
+      const fileContent = readFileSync(file).toString()
+      const document = load(fileContent)
+  
+      writeFileSync(file, document.html())
+  
+      tl.debug(`Uploading report`)
+      const attachmentProperties = {
+        name: generateName(basename(file)),
+        type: 'report-html'
+      }
+  
+      fileProperties.push(attachmentProperties)
+      tl.command('task.addattachment', attachmentProperties, file)
+    })
+  
+    const summaryPath = resolve(join(reportDir,'report.html'))
+    writeFileSync(summaryPath, JSON.stringify(fileProperties))
+    console.log(summaryPath)
+  
+    tl.command('task.addattachment', { name: generateName('report.html'), type: 'report-html'}, summaryPath)
+  }
+
+  function generateName (fileName) {
     const jobName = dashify(tl.getVariable('Agent.JobName'))
     const stageName = dashify(tl.getVariable('System.StageDisplayName'))
     const stageAttempt = tl.getVariable('System.StageAttempt')
     const tabName = tl.getInput('tabName', false ) || 'Html-Report'
-    let path = resolve(reportDir)
-    console.log(path)
-    tl.addAttachment('report-html', `${tabName}.${jobName}.${stageName}.${stageAttempt}`, path)  
+  
+    return `${tabName}.${jobName}.${stageName}.${stageAttempt}.${fileName}`
+  }
+
+try {
+    run()
 } catch (error) {
     tl.setResult(tl.TaskResult.SucceededWithIssues, error.message);
 }
