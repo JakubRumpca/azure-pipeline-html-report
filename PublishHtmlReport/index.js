80c20514cc3c244c3f50e1976949edcba729d1cf
Rumpca <Jakub.Rumpca@finastra.com>
2021-01-08 13:40:16 +0100

    version 1.0.6

--- PublishHtmlReport/index.js
+++ PublishHtmlReport/index.js
@@ -32,9 +32,6 @@ function run () {
     const stageName = dashify(tl.getVariable('System.StageDisplayName'))
     const stageAttempt = tl.getVariable('System.StageAttempt')
     const tabName = tl.getInput('tabName', false ) || 'Html-Report'
-    // let path = resolve(reportDir)
-    // console.log(path)
-    // tl.addAttachment('report-html', `${tabName}.${jobName}.${stageName}.${stageAttempt}`, path)  
     const summaryPath = resolve(reportDir)
     writeFileSync(summaryPath, JSON.stringify(fileProperties))
     console.log(summaryPath)
@@ -50,7 +47,14 @@ function generateName (fileName) {
   }
 
 try {
-    run()
+    let reportDir = tl.getPathInput('reportDir', true, true);
+    const jobName = dashify(tl.getVariable('Agent.JobName'))
+    const stageName = dashify(tl.getVariable('System.StageDisplayName'))
+    const stageAttempt = tl.getVariable('System.StageAttempt')
+    const tabName = tl.getInput('tabName', false ) || 'Html-Report'
+    let path = resolve(reportDir)
+    console.log(path)
+    tl.addAttachment('report-html', `${tabName}.${jobName}.${stageName}.${stageAttempt}`, path)  
 } catch (error) {
     tl.setResult(tl.TaskResult.SucceededWithIssues, error.message);
 }
